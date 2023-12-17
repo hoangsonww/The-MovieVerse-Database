8892d58a3099397f0a1f01e49b45543322a7c6dd
Son Nguyen <sonnh15@vng.com.vn>
2023-12-17 14:54:49 +0700

    Updated backend logic

---
+++ backend/settings.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = [
+    'rest_framework',
+    'movies',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfile,
+]
