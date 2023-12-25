67ae469c9cd00fb3cab8a07500481db415d6f5f0
Son Nguyen Hoang <hoangson091104@gmail.com>
2023-12-25 14:27:25 +0700

    Updated vue

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Welcome to The MovieVerse</h1>
+    </header>
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+    <footer>
+      <p>&copy; 2023 The MovieVerse. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
