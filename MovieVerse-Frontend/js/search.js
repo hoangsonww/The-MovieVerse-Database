fa910d231c3d09f9b3fdb0690fd6c1b4005e1297
Son Nguyen Hoang <hoangson091104@gmail.com>
2024-02-24 23:12:28 -0500

    Fix: Enhanced search feature (#39)

--- MovieVerse-Frontend/js/search.js
+++ MovieVerse-Frontend/js/search.js
@@ -273,7 +273,7 @@ async function showResults(category) {
     }
 
     updateBrowserURL(searchQuery);
-    document.title = `Search Results for ${searchQuery} - The MovieVerse`;
+    document.title = `Search Results for "${searchQuery}" - The MovieVerse`;
 }
 
 document.querySelector('button[onclick="showResults(\'movie\')"]').addEventListener('click', function() {
