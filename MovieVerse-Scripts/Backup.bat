95fa37ec38c6f4b63616ce603c34b5c5de73eb2b
Son Nguyen Hoang <hoangson091104@gmail.com>
2024-03-27 19:24:11 -0400

    Update: Enhanced UI layout & design (#111)

--- MovieVerse-Scripts/Backup.bat
+++ MovieVerse-Scripts/Backup.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Backing up MovieVerse project files...
-xcopy C:\path\to\MovieVerse C:\path\to\backup\MovieVerse /e /h /k /y
+xcopy C:\path\to\MovieVerse C:\Webstorm-Projects\MovieVerse /e /h /k /y
 echo Backup completed successfully.
