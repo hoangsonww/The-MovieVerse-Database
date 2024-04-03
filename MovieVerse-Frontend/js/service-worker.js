e3c7b93a8df78c4ee30525c2c05fd0567a23e69c
Son Nguyen Hoang <hoangson091104@gmail.com>
2024-04-03 19:38:43 -0400

    Update: Updated Sitemap & Dependencies (#150)

--- MovieVerse-Frontend/js/service-worker.js
+++ MovieVerse-Frontend/js/service-worker.js
@@ -4,7 +4,6 @@ const urlsToCache = [
     '../../index.html',
     '../css/style.css',
     '../css/trivia.css',
-    '../css/chatbot.css',
     '../../index.js',
     '../../manifest.json',
     'settings.js',
