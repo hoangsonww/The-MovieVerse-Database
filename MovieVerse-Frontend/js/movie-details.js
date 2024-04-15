044cf71286dde3bf3fc08ccb93729a17be5de888
Son Nguyen Hoang <hoangson091104@gmail.com>
2024-04-15 12:44:18 -0400

    Update: Major App Overhaul - Enhanced app translation (#159)

--- MovieVerse-Frontend/js/movie-details.js
+++ MovieVerse-Frontend/js/movie-details.js
@@ -1069,9 +1069,11 @@ async function populateMovieDetails(movie, imdbRating, rtRating, metascore, awar
         }
 
         return `<a href="${providerLink}" target="_blank" title="Watch on ${provider.provider_name}" style="display: inline-flex; align-items: flex-end; vertical-align: bottom;">
-            <img src="https://image.tmdb.org/t/p/original${provider.logo_path}" alt="${provider.provider_name}" style="width: 50px; margin-left: 10px;">
-        </a>`;
-    }).join('') : 'No streaming options available.';
+        <img src="https://image.tmdb.org/t/p/original${provider.logo_path}" alt="${provider.provider_name}" style="width: 50px; margin-left: 10px;">
+    </a>`;
+    }).join('') + `<a href="https://www.justwatch.com/us/search?q=${movieTitleEncoded}" target="_blank" title="View more streaming options on JustWatch" style="display: inline-flex; align-items: center; vertical-align: bottom; margin-left: 10px;">
+        <img src="../../images/justwatchlogo.png" alt="JustWatch" style="width: 50px;">
+    </a>` : 'No streaming options available.';
 
     const rtLink = rtRating !== 'N/A' ? `https://www.rottentomatoes.com/m/${getRtSlug(movie.title)}` : '#';
     const metaCriticsLink = metascore !== 'N/A' ? `https://www.metacritic.com/movie/${createMetacriticSlug(movie.title)}` : '#';
