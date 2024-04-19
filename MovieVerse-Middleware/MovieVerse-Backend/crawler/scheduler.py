2dfe176fed4dd5274457899452b576463d77dabf
Son Nguyen <hoangson091104@gmail.com>
2024-04-19 00:19:50 -0400

    Update: Updated project utilities (#159)

--- MovieVerse-Middleware/MovieVerse-Backend/crawler/scheduler.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-from apscheduler.schedulers.background import BackgroundScheduler
-from .tasks import crawl_movie_data_and_store
-
-scheduler = BackgroundScheduler()
-
-def start_scheduler():
-    scheduler.add_job(crawl_movie_data_and_store, 'interval', hours=24, args=['http://example.com/movies'])
-    scheduler.start()
