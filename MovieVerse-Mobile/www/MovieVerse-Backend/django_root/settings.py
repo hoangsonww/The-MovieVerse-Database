32ef067d4d698dfec942f8570036aaeb6d68d4e5
Son Nguyen Hoang <hoangson091104@gmail.com>
2024-04-23 23:34:41 -0400

    Update: Major App Overhaul - Enhanced app performance (#159)

--- MovieVerse-Mobile/www/MovieVerse-Backend/django_root/settings.py
+++ MovieVerse-Mobile/www/MovieVerse-Backend/django_root/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: remember to keep the secret key used in production secret!
-SECRET_KEY = 'replace_this_with_your_own_secret_key'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
