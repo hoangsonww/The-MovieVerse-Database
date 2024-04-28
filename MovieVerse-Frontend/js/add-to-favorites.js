5c55140fe8c763f06796c40260ca6917eb975b53
Son Nguyen Hoang <hoangson091104@gmail.com>
2024-04-28 12:14:27 -0400

    Update: Enhanced app performance (#159)

--- MovieVerse-Frontend/js/add-to-favorites.js
+++ MovieVerse-Frontend/js/add-to-favorites.js
@@ -1,52 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getFirestore, doc, setDoc, collection, updateDoc, query, where, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
-
-function translateFBC(value) {
-    return atob(value);
-}
-
-function getFBConfig1() {
-    const fbConfig1 = "QUl6YVN5REw2a1FuU2ZVZDhVdDhIRnJwS3VpdnF6MXhkWG03aw==";
-    return translateFBC(fbConfig1);
-}
-
-const movieCode = {
-    part1: 'YzVhMjBjODY=',
-    part2: 'MWFjZjdiYjg=',
-    part3: 'ZDllOTg3ZGNjN2YxYjU1OA=='
-};
-
-function getFBConfig2() {
-    const fbConfig2 = "bW92aWV2ZXJzZS1hcHAuZmlyZWJhc2VhcHAuY29t";
-    return translateFBC(fbConfig2);
-}
-
-function getFBConfig3() {
-    const fbConfig3 = "bW92aWV2ZXJzZS1hcHAuYXBwc3BvdC5jb20=";
-    return translateFBC(fbConfig3);
-}
-
-function getFBConfig4() {
-    const fbConfig4 = "ODAyOTQzNzE4ODcx";
-    return translateFBC(fbConfig4);
-}
-
-function getFBConfig5() {
-    const fbConfig5 = "MTo4MDI5NDM3MTg4NzE6d2ViOjQ4YmM5MTZjYzk5ZTI3MjQyMTI3OTI=";
-    return translateFBC(fbConfig5);
-}
-
-const firebaseConfig = {
-    apiKey: getFBConfig1(),
-    authDomain: getFBConfig2(),
-    projectId: "movieverse-app",
-    storageBucket: getFBConfig3(),
-    messagingSenderId: getFBConfig4(),
-    appId: getFBConfig5()
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { app, db } from './firebase.js';
 
 export async function checkAndUpdateFavoriteButton() {
     let userEmail = localStorage.getItem('currentlySignedInMovieVerseUser');
