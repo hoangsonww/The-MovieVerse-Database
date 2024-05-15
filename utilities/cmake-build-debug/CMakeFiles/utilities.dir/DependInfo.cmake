4b99274f59c09cdcf9422cc95453964bfe4fa01c
Son Nguyen <hoangson091104@gmail.com>
2024-05-15 07:37:40 +0700

    Update: Added C Files (#192)

---
+++ utilities/cmake-build-debug/CMakeFiles/utilities.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/hoang/WebstormProjects/The-MovieVerse-Database/utilities/applyFilterToVideo.c" "CMakeFiles/utilities.dir/applyFilterToVideo.c.obj" "gcc" "CMakeFiles/utilities.dir/applyFilterToVideo.c.obj.d"
+  "C:/Users/hoang/WebstormProjects/The-MovieVerse-Database/utilities/colorInvertingVideo.c" "CMakeFiles/utilities.dir/colorInvertingVideo.c.obj" "gcc" "CMakeFiles/utilities.dir/colorInvertingVideo.c.obj.d"
+  "C:/Users/hoang/WebstormProjects/The-MovieVerse-Database/utilities/imageEnhancer.c" "CMakeFiles/utilities.dir/imageEnhancer.c.obj" "gcc" "CMakeFiles/utilities.dir/imageEnhancer.c.obj.d"
+  "C:/Users/hoang/WebstormProjects/The-MovieVerse-Database/utilities/imageFilter.c" "CMakeFiles/utilities.dir/imageFilter.c.obj" "gcc" "CMakeFiles/utilities.dir/imageFilter.c.obj.d"
+  "C:/Users/hoang/WebstormProjects/The-MovieVerse-Database/utilities/redrawImage.c" "CMakeFiles/utilities.dir/redrawImage.c.obj" "gcc" "CMakeFiles/utilities.dir/redrawImage.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ utilities/cmake-build-debug/CMakeFiles/utilities.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utilities.dir/applyFilterToVideo.c.obj"
+  "CMakeFiles/utilities.dir/applyFilterToVideo.c.obj.d"
+  "CMakeFiles/utilities.dir/colorInvertingVideo.c.obj"
+  "CMakeFiles/utilities.dir/colorInvertingVideo.c.obj.d"
+  "CMakeFiles/utilities.dir/imageEnhancer.c.obj"
+  "CMakeFiles/utilities.dir/imageEnhancer.c.obj.d"
+  "CMakeFiles/utilities.dir/imageFilter.c.obj"
+  "CMakeFiles/utilities.dir/imageFilter.c.obj.d"
+  "CMakeFiles/utilities.dir/redrawImage.c.obj"
+  "CMakeFiles/utilities.dir/redrawImage.c.obj.d"
+  "libutilities.dll.a"
+  "utilities.exe"
+  "utilities.exe.manifest"
+  "utilities.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/utilities.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
