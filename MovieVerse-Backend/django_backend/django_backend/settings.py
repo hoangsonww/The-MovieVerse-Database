c983161400e27a5007d7a7476deaa08889f3bc9f
Son Nguyen <hoangson091104@gmail.com>
2024-06-17 15:54:35 +0700

    Final: Enhanced app's loading times (#196)

--- MovieVerse-Backend/django_backend/django_backend/settings.py
+++ MovieVerse-Backend/django_backend/django_backend/settings.py
@@ -20,10 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g$0^s=z6pbvyg9uas@!e8v8o-0&mb1u05sm3)a%@&4hc_hkb2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
