c404aff61234650021abdd0a5e272f847afafa19
Son Nguyen <hoangson091104@gmail.com>
2024-06-18 11:23:37 +0700

    Final: Enhanced app's loading times (#196)

--- MovieVerse-Backend/django_backend/django_backend/settings.py
+++ MovieVerse-Backend/django_backend/django_backend/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'movie-verse.com', 'www.movie-verse.com']
 
 # Application definition
 
