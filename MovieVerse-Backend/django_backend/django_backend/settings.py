7947662cef4ac25ccbeec6f743a0a6535d8dc300
Son Nguyen <hoangson091104@gmail.com>
2024-06-19 23:04:54 +0700

    Fix: Updated Backend

--- MovieVerse-Backend/django_backend/django_backend/settings.py
+++ MovieVerse-Backend/django_backend/django_backend/settings.py
@@ -154,7 +154,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
