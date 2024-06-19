0a38b6b432f8bc938c97a6eab983ddd027a59298
Son Nguyen <hoangson091104@gmail.com>
2024-06-19 23:58:40 +0700

    Final: Enhanced app's loading times (#196)

---
+++ MovieVerse-Mobile/www/MovieVerse-Config/eslint.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        indent: [
+            'error',
+            4,
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        quotes: [
+            'error',
+            'single',
+        ],
+        semi: [
+            'error',
+            'always',
+        ],
+        'react/prop-types': 0,
+    },
+};
