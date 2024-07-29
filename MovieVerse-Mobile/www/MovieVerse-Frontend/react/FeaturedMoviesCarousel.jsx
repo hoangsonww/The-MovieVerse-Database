3378d58dd4683bbd58f1eee0f4be175f257c837a
Son Nguyen <hoangson091104@gmail.com>
2024-07-29 12:27:38 +0700

    Final: Enhance app functionalities (#196)

--- MovieVerse-Mobile/www/MovieVerse-Frontend/react/FeaturedMoviesCarousel.jsx
+++ MovieVerse-Mobile/www/MovieVerse-Frontend/react/FeaturedMoviesCarousel.jsx
@@ -1,65 +1,61 @@
 import React, { Component } from 'react';
 
 class FeaturedMoviesCarousel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            featuredMovies: [],
-            loading: true,
-            error: null,
-            currentIndex: 0
-        };
-    }
-
-    componentDidMount() {
-        fetch('https://movie-verse.com/featured-movies')
-            .then(response => response.json())
-            .then(data => this.setState({ featuredMovies: data, loading: false }))
-            .catch(error => this.setState({ error, loading: false }));
-    }
-
-    goToPrevious = () => {
-        this.setState(prevState => ({
-            currentIndex: (prevState.currentIndex - 1 + prevState.featuredMovies.length) % prevState.featuredMovies.length
-        }));
+  constructor(props) {
+    super(props);
+    this.state = {
+      featuredMovies: [],
+      loading: true,
+      error: null,
+      currentIndex: 0,
     };
+  }
 
-    goToNext = () => {
-        this.setState(prevState => ({
-            currentIndex: (prevState.currentIndex + 1) % prevState.featuredMovies.length
-        }));
-    };
+  componentDidMount() {
+    fetch('https://movie-verse.com/featured-movies')
+      .then(response => response.json())
+      .then(data => this.setState({ featuredMovies: data, loading: false }))
+      .catch(error => this.setState({ error, loading: false }));
+  }
 
-    renderCarousel() {
-        const { featuredMovies, currentIndex } = this.state;
-        const currentMovie = featuredMovies[currentIndex];
+  goToPrevious = () => {
+    this.setState(prevState => ({
+      currentIndex: (prevState.currentIndex - 1 + prevState.featuredMovies.length) % prevState.featuredMovies.length,
+    }));
+  };
 
-        return (
-            <div className="carousel">
-                <button onClick={this.goToPrevious}>Previous</button>
-                <div className="carousel-content">
-                    <h3>{currentMovie.title}</h3>
-                    <p>{currentMovie.description}</p>
-                    <p>{currentMovie.rating} / 5</p>
-                    <p>{currentMovie.runtime} minutes</p>
-                </div>
-                <button onClick={this.goToNext}>Next</button>
-            </div>
-        );
-    }
+  goToNext = () => {
+    this.setState(prevState => ({
+      currentIndex: (prevState.currentIndex + 1) % prevState.featuredMovies.length,
+    }));
+  };
 
-    render() {
-        const { loading, error } = this.state;
+  renderCarousel() {
+    const { featuredMovies, currentIndex } = this.state;
+    const currentMovie = featuredMovies[currentIndex];
 
-        if (loading) return <p>Loading...</p>;
-        if (error) return <p>Error loading featured movies.</p>;
+    return (
+      <div className="carousel">
+        <button onClick={this.goToPrevious}>Previous</button>
+        <div className="carousel-content">
+          <h3>{currentMovie.title}</h3>
+          <p>{currentMovie.description}</p>
+          <p>{currentMovie.rating} / 5</p>
+          <p>{currentMovie.runtime} minutes</p>
+        </div>
+        <button onClick={this.goToNext}>Next</button>
+      </div>
+    );
+  }
 
-        return (
-            <div>
-                {this.renderCarousel()}
-            </div>
-        );
-    }
+  render() {
+    const { loading, error } = this.state;
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error loading featured movies.</p>;
+
+    return <div>{this.renderCarousel()}</div>;
+  }
 }
 
 export default FeaturedMoviesCarousel;
