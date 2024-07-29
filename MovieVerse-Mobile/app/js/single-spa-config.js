c36708d2153ee2589adc338c6e3eea9e736dfed2
Son Nguyen <hoangson091104@gmail.com>
2024-07-29 12:39:20 +0700

    Final: Enhance app functionalities (#196)

--- MovieVerse-Mobile/app/js/single-spa-config.js
+++ MovieVerse-Mobile/app/js/single-spa-config.js
@@ -2,36 +2,15 @@ import * as singleSpa from 'single-spa';
 
 // Function to load microfrontends (apps)
 function loadApp(name, path, activeWhen, customProps = {}) {
-    singleSpa.registerApplication(
-        name,
-        () => System.import(path),
-        activeWhen,
-        customProps
-    );
+  singleSpa.registerApplication(name, () => System.import(path), activeWhen, customProps);
 }
 
-loadApp(
-    'navbar',
-    '/path-to-navbar-microfrontend/navbar.js',
-    () => true // always active
-);
+loadApp('navbar', '/navbar.js', () => true);
 
-loadApp(
-    'home',
-    '/path-to-home-microfrontend/home.js',
-    (location) => location.pathname === '' || location.pathname === '/'
-);
+loadApp('home', '/home.js', location => location.pathname === '' || location.pathname === '/');
 
-loadApp(
-    'movie-details',
-    '/path-to-movie-details-microfrontend/movie-details.js',
-    (location) => location.pathname.startsWith('/movie')
-);
+loadApp('movie-details', '/movie-details.js', location => location.pathname.startsWith('/movie'));
 
-loadApp(
-    'about',
-    '/path-to-about-microfrontend/about.js',
-    (location) => location.pathname.startsWith('/about')
-);
+loadApp('about', '/about.js', location => location.pathname.startsWith('/about'));
 
 singleSpa.start();
