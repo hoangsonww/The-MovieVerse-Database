558541c4872d30a4196b9d6734ef566892a321d7
Son Nguyen <hoangson091104@gmail.com>
2024-08-25 21:00:00 -0400

    Final: Enhance app functionalities (#196)

--- MovieVerse-Mobile/platforms/ios/www/MovieVerse-Scripts/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Set-up PATH and other environment variables
-EMSDK_QUIET=1 source /MovieVerse/MovieVerse_env.sh
-
-exec "$@"
--- MovieVerse-Mobile/platforms/ios/www/python/django_backend/movieverse/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MovieverseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'movieverse'
