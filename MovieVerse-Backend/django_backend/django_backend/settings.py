e5a812c97294717b689a2b5f57b49e78242a86b6
Son Nguyen <hoangson091104@gmail.com>
2024-09-02 10:05:46 -0400

    Final: Enhance app functionalities (#196)

--- MovieVerse-Backend/django_backend/django_backend/settings.py
+++ MovieVerse-Backend/django_backend/django_backend/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
